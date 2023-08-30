7392f75b59d5877a540baf698e7220ff55da13a6
kiennd <duykien85@gmail.com>
2023-08-30 10:53:54 +0700

    up

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,5 +1,7 @@
 import { Row, Tag, Checkbox } from 'antd';
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleTodoStatus } from '../../redux/actions';
 
 const priorityColorMapping = {
   High: 'red',
@@ -7,25 +9,33 @@ const priorityColorMapping = {
   Low: 'gray',
 };
 
-export default function Todo({ name, prioriry }) {
-  const [checked, setChecked] = useState(false);
+export default function Todo({ name, prioriry, completed, id }) {
+  const [checked, setChecked] = useState(completed);
 
+  const dispatch = useDispatch();
   const toggleCheckbox = () => {
     setChecked(!checked);
+    dispatch(toggleTodoStatus(id));
   };
 
   return (
     <Row
-      justify='space-between'
+      justify="space-between"
       style={{
         marginBottom: 3,
         ...(checked ? { opacity: 0.5, textDecoration: 'line-through' } : {}),
       }}
     >
-      <Checkbox checked={checked} onChange={toggleCheckbox}>
+      <Checkbox
+        checked={checked}
+        onChange={toggleCheckbox}
+      >
         {name}
       </Checkbox>
-      <Tag color={priorityColorMapping[prioriry]} style={{ margin: 0 }}>
+      <Tag
+        color={priorityColorMapping[prioriry]}
+        style={{ margin: 0 }}
+      >
         {prioriry}
       </Tag>
     </Row>
---
+++ src/redux/store.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux';
+import rootReducer from './reducer';
+
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+//Su dung dev tool extension
+const composeEnhancers = composeWithDevTools();
+
+const store = createStore(rootReducer, composeEnhancers);
+
+export default store;
+
+// import { configureStore } from '@reduxjs/toolkit';
+
+// import filtersSlice from '../components/Filters/filtersSlice';
+// import todoListSlice from '../components/TodoList/todoListSlice';
+
+// const store = configureStore({
+//   reducer: {
+//     filters: filtersSlice.reducer,
+//     todoList: todoListSlice.reducer,
+//   },
+// });
+
+// export default store;
